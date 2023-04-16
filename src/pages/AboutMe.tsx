c12d9c449bf69276ee54906041bfef2c0dbbc654
Luca's Lab <92208704+lucaslab-git@users.noreply.github.com>
2023-04-16 22:09:53 +0200

    1.0

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import avatar from "../images/avatar.jpg";
+import Name from "./Name";
+
+function AboutMe() {
+  const txtaboutme = `Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B
+    geschrieben haben, hält mich die faszination der Technology fest. 
+    In den letzten 8 Jahren habe ich vielzählige Projekte selbst entwickelt 
+    und oder betreut. Dabei hält sich meine Wlet nicht in Grenzen der Software 
+    auf, sondern auch in der Hardware bzw. tatsächlichen Umsetzung. 
+    Mit diversen Projekten wie z.B. einer eigensgebauten Rakete musste ich schon 
+    viele Hurden und Aufgaben auf mich nehmen.`;
+
+  const txtlucaslab = `
+  Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B+ 
+  geschrieben haben, hält mich die faszination der Technology fest. 
+  In den letzten 8 Jahren habe ich vielzählige Projekte selbst entwickelt 
+  und oder betreut. Dabei hält sich meine Wlet nicht in Grenzen der Software 
+  auf, sondern auch in der Hardware bzw. tatsächlichen Umsetzung. 
+  Mit diversen Projekten wie z.B. einer eigensgebauten Rakete musste ich schon 
+  viele Hurden und Aufgaben auf mich nehmen.`;
+  return (
+    <>
+      <div className="Comment_Icon">
+        <img src={avatar} className="Student_Icon"></img>
+
+        <div className="Comments">
+          <Name />
+        </div>
+      </div>
+
+      <div className="txt-aboutme">
+        <h3>About Me</h3>
+        <p>{txtaboutme}</p>
+        <br />
+        <h3>Lucas Lab</h3>
+        <p>{txtlucaslab}</p>
+      </div>
+    </>
+  );
+}
+export default AboutMe;
---
+++ src/pages/Name.tsx
@@ -0,0 +1,23 @@
+import Typewriter from "typewriter-effect";
+import * as React from "react";
+
+function Name() {
+  return (
+    <>
+      <div className="aboutme">
+        Hi, ich bin Luca Schweda
+        <div className="typwrt">
+          <Typewriter
+            options={{
+              strings: ["Entwickler", "Coder", "Filmmaker"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Name;
