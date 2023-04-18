26ed0806038fd40b90ae07f90ddc0bdf073a8614
Luca's Lab <92208704+lucaslab-git@users.noreply.github.com>
2023-04-18 20:42:17 +0200

    10.1

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,76 +1,23 @@
 import * as React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import avatar from "../images/avatar.jpg";
+import Github from "../images/Github.png";
+import YT from "../images/YT.png";
+import Instagram from "../images/Instagram.svg";
 import Name from "./Name";
 
-const App = () => {
-  const [width, setWidth] = React.useState(
-    typeof window !== "undefined" ? window.innerWidth : null
-  );
-  const breakpoint = 700;
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      // add window resize event listener
-      const handleResize = () => setWidth(window.innerWidth);
-      window.addEventListener("resize", handleResize);
-      // remove window resize event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
-    }
-  }, []);
-  if (width > breakpoint) {
-    return (
-      <div className="btnbox">
-        <button type="button" className="btn">
-          Click Me
-        </button>
-        <button type="button" className="btn">
-          Click Me
-        </button>
-        <button type="button" className="btn">
-          Click Me
-        </button>
-        <button type="button" className="btn">
-          Click Me
-        </button>
-        <button type="button" className="btn">
-          Click Me
-        </button>
-        <button type="button" className="btn">
-          Click Me
-        </button>
-      </div>
-    );
-  }
-  return (
-    <>
-      <div className="btnbox-sm">
-        <button type="button" className="btn sm"></button>
-        <button type="button" className="btn sm"></button>
-        <button type="button" className="btn sm"></button>
-        <button type="button" className="btn sm"></button>
-        <button type="button" className="btn sm"></button>
-        <button type="button" className="btn sm"></button>
-      </div>
-      <div className="hdproj">
-        <h1>
-          <u>Projekte</u>
-        </h1>
-      </div>
-    </>
-  );
-};
-
 function AboutMe() {
-  const txtaboutme = `Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B
-    geschrieben haben, hält mich die faszination der Technology fest. 
-    In den letzten 8 Jahren habe ich vielzählige Projekte selbst entwickelt 
-    und oder betreut. `;
-
-  const txtlucaslab = `
-  Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B+ 
+  const txtaboutme = `Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B+ 
   geschrieben haben, hält mich die faszination der Technology fest. 
   In den letzten 8 Jahren habe ich vielzählige Projekte selbst entwickelt 
   und oder betreut. `;
+
+  const txtlucaslab = `
+Als ich 2015 mein erstes Progeamm auf dem Einplatienencomputer RPI3B+ 
+geschrieben haben, hält mich die faszination der Technology fest. 
+In den letzten 8 Jahren habe ich vielzählige Projekte selbst entwickelt 
+und oder betreut. Dabei hält sich meine Wlet nicht in Grenzen der Software 
+auf, sondern auch in der Hardware bzw. `;
 
   return (
     <>
@@ -89,7 +36,32 @@ function AboutMe() {
         <h3>Lucas Lab</h3>
         <p>{txtlucaslab}</p>
       </div>
-      <App></App>
+
+      <Container className="cont-btn">
+        <Row className="Row">
+          <Col>
+            <button type="button" className="btn">
+              <img src={Github} className="ico"></img>
+            </button>
+
+            <button type="button" className="btn">
+              <img src={YT} className="ico"></img>
+            </button>
+
+            <button type="button" className="btn">
+              <img src={Instagram} className="ico"></img>
+            </button>
+
+            <button type="button" className="btn">
+              <img src={YT} className="ico"></img>
+            </button>
+
+            <button type="button" className="btn">
+              <img src={Instagram} className="ico"></img>
+            </button>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
